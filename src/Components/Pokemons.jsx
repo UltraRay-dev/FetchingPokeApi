7fc6ceedaf2886a390e17e203ac87047699abb4c
Jose <148951093+UltraRay-dev@users.noreply.github.com>
2023-10-31 10:00:04 +0100

    Update Pokemons.jsx

--- src/Components/Pokemons.jsx
+++ src/Components/Pokemons.jsx
@@ -20,7 +20,7 @@ function PokemonInfo({ pokemon }) {
 
 function ObtenerPokemons() {
   const [pokemons, setPokemons] = useState([]);
-  const numeroDePokemons = 10; // Cantidad de pokemons que se desean cargar
+  const numeroDePokemons = 16; // Cantidad de pokemons que se desean cargar
 
   useEffect(() => {
     async function fetchPokemonData(numeroDePokemons) {
